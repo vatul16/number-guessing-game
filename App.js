9d44a1770c7d193e13cb69eb1c184d862a631b40
Atul Vishwakarma <vishwa.atul16@gmail.com>
2024-05-30 12:53:58 +0530

    Number Guessing Game

---
+++ App.js
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { StyleSheet, View, ImageBackground, SafeAreaView } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import AppLoading from "expo-app-loading";
+
+import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
+import GameOverScreen from "./screens/GameOverScreen";
+
+import Colors from "./constants/colors";
+
+export default function App() {
+  const [userNumber, setUserNumber] = useState();
+  const [gameIsOver, setGameIsOver] = useState(true);
+  const [guessRounds, setGuessRounds] = useState(0);
+
+  const [fontsLoaded] = useFonts({
+    "open-sans-regular": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+    setGameIsOver(false);
+  }
+
+  function gameOverHandler(numberOfRounds) {
+    setGameIsOver(true);
+    setGuessRounds(numberOfRounds);
+  }
+
+  function startNewGameHandler() {
+    setUserNumber(null);
+    setGuessRounds(0);
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+
+  if (userNumber) {
+    screen = (
+      <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />
+    );
+  }
+
+  if (gameIsOver && userNumber) {
+    screen = (
+      <GameOverScreen
+        userNumber={userNumber}
+        roundsNumber={guessRounds}
+        onStartNewGame={startNewGameHandler}
+      />
+    );
+  }
+
+  return (
+    <>
+    <StatusBar style="light" /> 
+      <View style={styles.rootScreen}>
+        <LinearGradient
+          colors={[Colors.primary700, Colors.ascent500]}
+          style={styles.rootScreen}
+        >
+          <ImageBackground
+            source={require("./assets/images/background.png")}
+            resizeMode="cover"
+            style={styles.rootScreen}
+            imageStyle={styles.backgroundImage}
+          >
+            <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+          </ImageBackground>
+        </LinearGradient>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  },
+});
---
+++ components/ui/InstructionText.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text } from "react-native";
+
+import Colors from "../../constants/colors";
+
+function InstructionText({children, style}) {
+  return <Text style={[styles.instructionText, style]}>{children}</Text>;
+}
+
+export default InstructionText;
+
+const styles = StyleSheet.create({
+  instructionText: {
+    fontFamily: "open-sans-regular",
+    color: Colors.ascent500,
+    fontSize: 24,
+  },
+});
